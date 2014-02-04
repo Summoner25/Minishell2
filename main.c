616875da4a2b072ab712d2f7c291276683f81c22
courci_b <courci_b@epitech.eu>
2014-02-04 13:30:37 +0100

    new files

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+** main.c for main.c in /home/courci_b/github/minishell2
+** 
+** Made by Marvin
+** Login   <courci_b@epitech.net>
+** 
+** Started on  Tue Feb  4 13:29:08 2014 Marvin
+** Last update Tue Feb  4 13:30:00 2014 Marvin
+*/
+
+int	main(int ac, char **av)
+{
+  char	*str;
+
+  str = "Salut";
+  printf("%s\n", str);
+}
